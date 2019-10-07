0420af870f9ba497e08ddb48ef74f3fb56a836d3
adityamanocha <aditya.manocha@gmail.com>
2019-10-07 21:02:11 +0530

    Initial Commit

---
+++ src/main/java/com/javaproj/javaproj_liquidemo/App.java
@@ -0,0 +1,13 @@
+package com.javaproj.javaproj_liquidemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
