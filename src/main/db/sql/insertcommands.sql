edb3c83d52dd585e7a024bc7a0df1f0f8c462881
adityamanocha <aditya.manocha@gmail.com>
2019-10-08 13:11:51 +0530

    More inserts

--- src/main/db/sql/insertcommands.sql
+++ src/main/db/sql/insertcommands.sql
@@ -2,3 +2,7 @@
 
 --changeset devopsadmin:change_2
 INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (1, "Aditya", 99606496);
+
+--changeset devopsadmin:change_3
+INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (2, "Manocha", 99606497);
+INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (3, "Pawan", 99606494);
