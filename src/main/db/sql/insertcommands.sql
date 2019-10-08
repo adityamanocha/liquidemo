3d4d52e4981aedc7d4179fee0fb59b589f3dd2d8
adityamanocha <aditya.manocha@gmail.com>
2019-10-08 10:44:49 +0530

    Insert statement sql

---
+++ src/main/db/sql/insertcommands.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset {devopsadmin}:{change_2}
+
+
+INSERT INTO emp (EmpId, EmpName, EmpMob)
+VALUES (1, "Aditya", 99606496);
+
+
+INSERT INTO emp (EmpId, EmpName, EmpMob)
+VALUES (2, "Mano", 99606497);
