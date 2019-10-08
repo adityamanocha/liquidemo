6cd8295e002fe2f726eeb47f9a25aa227cda7270
adityamanocha <aditya.manocha@gmail.com>
2019-10-08 12:29:30 +0530

    Updated sql, changelog and properties

--- src/main/db/sql/insertcommands.sql
+++ src/main/db/sql/insertcommands.sql
@@ -1,7 +1,6 @@
 --liquibase formatted sql
+
 --changeset devopsadmin:change_2
-
-
-INSERT INTO emp(EmpId, EmpName, EmpMob) 
-VALUES (1, "Aditya", 99606496),
-VALUES (2, "Mano", 99606497);
+INSERT INTO emp(EmpId, EmpName, EmpMob)
+VALUES (1, "Aditya", 99606496);
+VALUES (2, "Manocha", 99606497);
