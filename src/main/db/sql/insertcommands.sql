bb4d17e8312fac770e7946bce0835cd14d45752d
adityamanocha <aditya.manocha@gmail.com>
2019-10-08 13:52:22 +0530

    Added update statement

--- src/main/db/sql/insertcommands.sql
+++ src/main/db/sql/insertcommands.sql
@@ -6,3 +6,7 @@ INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (1, "Aditya", 99606496);
 --changeset devopsadmin:change_3
 INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (2, "Manocha", 99606497);
 INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (3, "Pawan", 99606494);
+
+
+--changeset devopsadmin:change_4
+UPDATE emp SET EmpMob=99606495 WHERE EmpId=3;
