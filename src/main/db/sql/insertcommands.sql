449cb3f0b853d6d539adc589dae77ec59b820c41
adityamanocha <aditya.manocha@gmail.com>
2019-10-09 11:45:06 +0530

    Added insert statements

--- src/main/db/sql/insertcommands.sql
+++ src/main/db/sql/insertcommands.sql
@@ -10,3 +10,10 @@ INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (3, "Pawan", 99606494);
 
 --changeset devopsadmin:change_4
 UPDATE emp SET EmpMob=99606495 WHERE EmpId=3;
+
+
+--changeset devopsadmin:change_5
+INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (4, "Shabaz", 99606497);
+INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (5, "ABC", 99606494);
+INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (6, "PQR", 99606497);
+INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (7, "XYZ", 99606494);
