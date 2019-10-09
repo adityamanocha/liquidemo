3511ce03f83362c2e19bf2f0673cfcfe7af20a72
adityamanocha <aditya.manocha@gmail.com>
2019-10-09 14:30:58 +0530

    Added stored procedure

--- src/main/db/sql/insertcommands.sql
+++ src/main/db/sql/insertcommands.sql
@@ -17,3 +17,10 @@ INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (4, "Shabaz", 99606497);
 INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (5, "ABC", 99606494);
 INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (6, "PQR", 99606497);
 INSERT INTO emp(EmpId, EmpName, EmpMob) VALUES (7, "XYZ", 99606494);
+
+
+--changeset devopsadmin:change_6
+CREATE PROCEDURE GetAllEmp()
+BEGIN
+    SELECT *  FROM emp;
+END
